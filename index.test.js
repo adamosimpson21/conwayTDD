bf7ae2777a23576f60b77451a91d20f96abdb0e5
BandsWithLegends <adamosimpson21@gmail.com>
2019-03-29 22:40:01 -0700

    First tests with input

--- index.test.js
+++ index.test.js
@@ -7,3 +7,7 @@ test("Input [[0]] returns [[0]]", () =>{
 test("Input [[1]] returns [[0]]", () =>{
   expect(gameOfLife([[1]])).toEqual([[0]])
 })
+
+test("Input [[0, 0], [0, 0]] returns [[0, 0], [0, 0]]", () =>{
+  expect(gameOfLife([[0, 0], [0, 0]])).toEqual([[0, 0], [0, 0]])
+})
