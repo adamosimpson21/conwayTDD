cccaf276738d09d8eea1fd318417f8f4976297a5
BandsWithLegends <adamosimpson21@gmail.com>
2019-03-29 23:04:56 -0700

    Solved for rows <=2

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const gameOfLife = board => {
   if(board.length ===2){
-    let counter = 0
-    const aliveCells = board.forEach(row => {
-      row.forEach(cell => cell && ++counter )
-    })
-    if(aliveCells <2){
+    let aliveCells = 0
+    board.forEach(row => row.forEach(cell => cell && ++aliveCells))
+    if(aliveCells <=2){
       return [[0,0], [0,0]]
+    } else if(aliveCells ===3){
+      return [[1,1],[1,1]]
+    } else {
+      return board;
     }
-    return board;
   }
   return [[0]];
 }
--- index.test.js
+++ index.test.js
@@ -19,3 +19,23 @@ test("Input [[1, 1], [1, 1]] returns [[1, 1], [1, 1]]", () =>{
 test("Input [[1, 0], [0, 1]] returns [[0, 0], [0, 0]]", () =>{
   expect(gameOfLife([[1, 0], [0, 1]])).toEqual([[0, 0], [0, 0]])
 })
+
+test("Input [[1, 0], [1, 1]] returns [[1, 1], [1, 1]]", () =>{
+  expect(gameOfLife([[1, 0], [1, 1]])).toEqual([[1, 1], [1, 1]])
+})
+
+test("Input [[1, 1], [1, 0]] returns [[1, 1], [1, 1]]", () =>{
+  expect(gameOfLife([[1, 1], [1, 0]])).toEqual([[1, 1], [1, 1]])
+})
+
+test("Input [[0, 1], [1, 1]] returns [[1, 1], [1, 1]]", () =>{
+  expect(gameOfLife([[0, 1], [1, 1]])).toEqual([[1, 1], [1, 1]])
+})
+
+test("Input [[0, 0], [0, 1]] returns [[0, 0], [0, 0]]", () =>{
+  expect(gameOfLife([[0, 0], [0, 1]])).toEqual([[0, 0], [0, 0]])
+})
+
+test("Input [[0, 1], [0, 0]] returns [[0, 0], [0, 0]]", () =>{
+  expect(gameOfLife([[0, 1], [0, 0]])).toEqual([[0, 0], [0, 0]])
+})
