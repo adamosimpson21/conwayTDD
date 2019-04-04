f3641178fcf7181e00cd2d73bff6abc53358645f
BandsWithLegends <adamosimpson21@gmail.com>
2019-04-04 02:13:15 -0700

    MORE TESTS

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ const gameOfLife = board => {
 }
 
 const getNumNeighbors = (x, y, board) => {
-  return 0;
+  let numLiveneighbors = 0;
+  const neighbors = [
+                      [x-1,y-1],[x-1,y  ],[x-1,y+1],
+                      [x  ,y-1],          [x  ,y+1],
+                      [x+1,y-1],[x+1,y  ],[x+1,y+1]
+                    ]
+  neighbors.forEach(neighbor => board[neighbor[0]] && board[neighbor[0]][neighbor[1]] && ++numLiveneighbors);
+  return numLiveneighbors;
 }
 
 const oneOrZero = () => {
@@ -25,6 +32,16 @@ const constructBoard = number =>{
   return Array(number).fill(null).map(() => Array(number).fill(null).map(() => oneOrZero()));
 }
 
+const countAllNeighbors = board => {
+  let neighborBoard = Array(board.length).fill(null).map(() => Array(board.length).fill(null));
+  board.forEach((row, y) => {
+    row.forEach((cell, x) => {
+      neighborBoard[x][y] = getNumNeighbors(x, y, board);
+    })
+  })
+  return neighborBoard;
+}
+
 // constructBoard(10);
 
-export {gameOfLife, constructBoard, oneOrZero, getNumNeighbors};
+export {gameOfLife, constructBoard, oneOrZero, getNumNeighbors, countAllNeighbors};
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import {gameOfLife, constructBoard, oneOrZero, getNumNeighbors} from "./index.js"
+import {gameOfLife, constructBoard, oneOrZero, getNumNeighbors, countAllNeighbors} from "./index.js"
 
 test("Input [[0]] returns [[0]]", () =>{
   expect(gameOfLife([[0]])).toEqual([[0]])
@@ -81,12 +81,10 @@ const mockboard = [
   [1, 1, 1, 1]
 ]
 
-
 // testing getNumNeighbors
 test("getNumNeighbors return a number", () => {
   expect(typeof getNumNeighbors(0, 0, mockboard)).toBe("number");
 })
-
 
 test("getNumNeighbors(1,1) returns 5", () => {
   expect(getNumNeighbors(1, 1, mockboard)).toBe(5);
@@ -98,4 +96,45 @@ test("getNumNeighbors(2,2) returns 6", () => {
 
 test("getNumNeighbors(0,0) returns 1", () => {
   expect(getNumNeighbors(0, 0, mockboard)).toBe(1);
+})
+
+test("getNumNeighbors(3,0) returns 2", () => {
+  expect(getNumNeighbors(3, 0, mockboard)).toBe(2);
+})
+
+// count all neighbors tests
+
+test("countAllNeighbors returns a 2D array", () => {
+  expect(typeof countAllNeighbors(mockboard)[0][0]).toBe("number")
+})
+
+test("countAllNeighbors returns a 2D array of the same size", () => {
+  expect(countAllNeighbors(mockboard).length).toBe(4)
+})
+
+test("countAllNeighbors returns cells with a value over 1 v1", () => {
+  let max = 0;
+  const numNeighborBoard = countAllNeighbors(mockboard);
+  numNeighborBoard.forEach(row => row.forEach(cell => (cell>max) && (max = cell)))
+  expect(max).toBe(6)
+})
+
+test("countAllNeighbors returns cells with a value over 1 v2", () => {
+  let maxInRow = [];
+  countAllNeighbors(mockboard).forEach(row => maxInRow.push(Math.max(...row)));
+  expect(Math.max(...maxInRow)).toBe(6)
+})
+
+test("countAllNeighbors returns cells with a value over 1 v2", () => {
+  let maxInRow = [];
+  countAllNeighbors(mockboard).forEach(row => maxInRow.push(Math.max(...row)));
+  expect(Math.max(...maxInRow)).toBe(6)
+})
+
+test("countAllNeighbors[1][3] returns 4", () => {
+  expect(countAllNeighbors(mockboard)[1][3]).toBe(4)
+})
+
+test("countAllNeighbors[0][0] returns 1", () => {
+  expect(countAllNeighbors(mockboard)[0][0]).toBe(1)
 })
